b620dc9a66b8e30fb7348732027fa220db08b92a
brunolimapinheiro <brunol21082001@gmail.com>
2024-08-25 14:26:26 -0300

    fiz algumas alterações e criei o modal cadastro

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -3,12 +3,20 @@ import { Main } from "../../components/Main/Main";
 import { useState } from "react";
 import ModalLogin from "../../components/modal/Modal";
 import ModalForm from "../../components/modal/ModalForm";
+import ModalCadastre from "../../components/modal/ModalCadastre";
 
 export default function Home() {
   const [open, setOpen] = useState(false);
   const [openForm, setOpenForm] = useState(false);
+  const [openCadastre, setOpenCadastre] = useState(false);
   const handleOpen = () => setOpen(!open);
   const handleClose = () => setOpenForm(false);
+  const handOpenNext = () => {
+    setOpenForm(false);
+    setTimeout(() => {
+      setOpenCadastre(true);
+    }, 2000);
+  };
   const handModalForm = () => {
     if (open) {
       setOpen(false);
@@ -26,7 +34,12 @@ export default function Home() {
         handModalForm={handModalForm}
       />
 
-      <ModalForm open={openForm} handleOpen={handleClose} />
+      <ModalForm
+        open={openForm}
+        handleOpen={handleClose}
+        handOpenNext={handOpenNext}
+      />
+      <ModalCadastre open={openCadastre} handleOpen={handleClose} />
 
       <Main />
     </>
